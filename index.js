4d9c64fc9a47b17707695bab3bdae6a065302134
Dinobergue Viana <dinobergueviana@gmail.com>
2020-07-23 09:10:15 -0300

    cria a função adicionarAluno

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 // Base a ser utilizada
 const alunosDaEscola=[{nome:"Henrique",notas:[],cursos:[],faltas:5},{nome:"Edson",notas:[],cursos:[],faltas:2},{nome:"Bruno",notas:[10,9.8,9.6],cursos:[],faltas:0},{nome:"Guilherme",notas:[10,9.8,9.6],cursos:[{nomeDoCurso:"Full Stack",dataMatricula:new Date}],faltas:0},{nome:"Carlos",notas:[],cursos:[],faltas:0},{nome:"Lucca",notas:[10,9.8,9.6],cursos:[{nomeDoCurso:"UX",dataMatricula:new Date}],faltas:0}];
 
-
 // implementação
+
+// adiciona um novo aluno;
+function adicionarAluno(nome){
+    let aluno = {
+        nome: nome,
+        notas: [],
+        curso: [],
+        faltas: 0
+    }
+    console.log("Aluno adcionado com sucesso!")
+    alunosDaEscola.push(aluno);
+}
